0c4afff6904aad626cc652d0b6cc358cd14d95ac
Pozelaite <jana.pozelaite@accenture.com>
2019-02-12 08:35:58 +0200

    Users component added

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -22,11 +22,10 @@ class UserCard extends Component {
       //     <p className="card-text">{description}</p>
       //  </div>
       // </div>
-      <Media className="UserCard mb-3">
-          <Media className="UserCard__img media-left" width="100" object src={image} alt="Generic placeholder image" />
-        <Media body>
-          <h4 className="card-title">{title}</h4>
-          <p className="card-text">{description}</p>
+      <Media className="UserCard mb-3 d-flex align-items-center">
+          <Media className="UserCard__img media-left" width="70" object src={image} alt="Generic placeholder image" />
+        <Media body className="d-flex justify-content-start">
+          <h6 className="card-title ml-2">{title}</h6>
         </Media>
       </Media>
     )
